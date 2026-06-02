09ab77cc32bac5981b0708fb02473d461727f829
Mateo Cornejo <ccornejomateo@gmail.com>
2026-06-01 22:16:14 -0300

    icons updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
 				<meta name="mobile-web-app-capable" content="yes" />
 				<meta name="apple-mobile-web-app-capable" content="yes" />
 				<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-				<meta name="apple-mobile-web-app-title" content="AR Aberturas" />
+				<meta name="apple-mobile-web-app-title" content="Doceocho" />
 				<meta
 					name="viewport"
 					content="width=device-width, initial-scale=1, maximum-scale=5, user-scalable=yes"
