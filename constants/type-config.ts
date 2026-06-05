ede776a5f35fdc40bfd2f16b29d32a3a6dbb32bf
Mateo Cornejo <ccornejomateo@gmail.com>
2026-06-05 09:47:43 -0300

    A new status dictionary has been added to the calendar

--- constants/type-config.ts
+++ constants/type-config.ts
@@ -1,38 +1,4 @@
-import { User, Package, Wrench, MoreHorizontal } from 'lucide-react';
 import { Clock, CheckCircle, AlertCircle } from 'lucide-react';
-
-export const typeConfig = {
-	produccionOK: {
-		label: 'Producción OK',
-		icon: Package,
-		color: 'bg-chart-1 text-chart-1',
-		backgroundColor: '#0b96f3ff',
-	},
-	colocacion: {
-		label: 'Colocación',
-		icon: Wrench,
-		color: 'bg-chart-2 text-chart-2',
-		backgroundColor: '#0bf38fff',
-	},
-	medicion: {
-		label: 'Medición',
-		icon: User,
-		color: 'bg-chart-3 text-chart-3',
-		backgroundColor: '#f3a40bff',
-	},
-	reuniones: {
-		label: 'Reuniones',
-		icon: MoreHorizontal,
-		color: 'bg-chart-4 text-chart-4',
-		backgroundColor: 'rgb(212, 88, 183)',
-	},
-	otros: {
-		label: 'Otros',
-		icon: MoreHorizontal,
-		color: 'bg-gray-400 text-gray-400',
-		backgroundColor: '#a0a0a0ff',
-	},
-};
 
 export const statusOptions = [
 	{ value: 'pending', label: 'Pendiente', icon: Clock, color: 'text-gray-400' },
