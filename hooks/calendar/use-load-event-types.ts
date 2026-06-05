f6711f0ec10aba4e71f224d367bb2f6f11f159ab
Mateo Cornejo <ccornejomateo@gmail.com>
2026-06-05 09:48:54 -0300

    The event hook was adapted to retrieve the type from the  table

---
+++ hooks/calendar/use-load-event-types.ts
@@ -0,0 +1,25 @@
+import { EventType, listEventTypes } from '@/lib/calendar/event-types';
+import { useOptimizedRealtime } from '@/hooks/use-optimized-realtime';
+
+export function useLoadEventTypes() {
+	const {
+		data: eventTypes,
+		loading: isLoading,
+		error,
+		refresh,
+	} = useOptimizedRealtime<EventType>(
+		'events_types',
+		async () => {
+			const { data } = await listEventTypes();
+			return data ?? [];
+		},
+		'event_types_cache'
+	);
+
+	return {
+		eventTypes,
+		isLoading,
+		error,
+		refresh,
+	};
+}
