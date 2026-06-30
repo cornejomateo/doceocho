01e3ec32f2b42b1ae2865cc944edb0d28f05fa2a
Mateo Cornejo <ccornejomateo@gmail.com>
2026-06-29 23:22:07 -0300

    Remove unused event listener

--- public/sw.js
+++ public/sw.js
@@ -27,18 +27,6 @@ self.addEventListener('activate', (event) => {
 	);
 });
 
-// Fetch event - serve from cache, fallback to network
-self.addEventListener('fetch', (event) => {
-	event.respondWith(
-		caches.match(event.request).then((response) => {
-			if (response) {
-				return response;
-			}
-			return fetch(event.request);
-		})
-	);
-});
-
 // Push notification event
 self.addEventListener('push', (event) => {
 	if (!event.data) {
